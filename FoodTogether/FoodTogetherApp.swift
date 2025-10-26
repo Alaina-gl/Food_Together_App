ef9f4355898a545e131b304231313bf5e3ac48d3
Alaina-gl <108637123+Alaina-gl@users.noreply.github.com>
2025-10-25 17:06:45 -0700

    Initial Commit

---
+++ FoodTogether/FoodTogetherApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodTogetherApp.swift
+//  FoodTogether
+//
+//  Created by Alaina Ge on 2025-10-25.
+//
+
+import SwiftUI
+
+@main
+struct FoodTogetherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
